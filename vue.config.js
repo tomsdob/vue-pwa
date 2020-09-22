ba0c3ddfeab9d9ab6d7a4023108819cbbe987ef3
Toms D <tomy22783@gmail.com>
2020-09-22 08:40:54 +0300

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+// Inside vue.config.js
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: "vue-pwa",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "src/registerServiceWorker.js"
+      // ...other Workbox options...
+    },
+    manifestOptions: {
+      name: "pwa.name",
+      short_name: "pwa.name",
+      start_url: "/",
+      display: "standalone",
+      theme_color: "pwa.themeColor"
+    }
+  }
+};
